3d4df138818a45e47c3824923b4881d83fca32a7
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-04 23:07:24 +0800

    do #3, create a render function for the gameboard

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /*
     1. button to start the game
     2. store the gameboard as an array inside of a Gameboard object
-    3. 
+    3. Set up your HTML
+        write a JavaScript function that will render the contents of the gameboard array to the webpage
 
 
 
@@ -11,5 +12,20 @@
 */
 
 const Gameboard = (() => {
-    let gameboard = ['', '', '', '', '', '', '', '', ''];
+    const gameboard = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
+
+    const render = () => {
+        let consoleGameBoard = '';
+        for (let i = 0; i < 9; i++) {
+            consoleGameBoard += gameboard[i];
+            if (i == 2 || i == 5) {
+                consoleGameBoard += '\n';
+            }
+        }
+        console.log(consoleGameBoard);
+    };
+
+    return render;
 })();
+
+Gameboard();
