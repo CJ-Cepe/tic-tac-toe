5b920a029e4b3bf22ed5fce45fcad4e0519818e4
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-04 22:43:21 +0800

    do # 1 and 2 of the exercise

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+    1. button to start the game
+    2. store the gameboard as an array inside of a Gameboard object
+    3. 
+
+
+
+
+
+
+*/
+
+const Gameboard = (() => {
+    let gameboard = ['', '', '', '', '', '', '', '', ''];
+})();
