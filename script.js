e09ff07d541da754f916bbea7b95685c18badfdf
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-05 23:25:06 +0800

    add player object version 1

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-/*
-    1. button to start the game
-    2. store the gameboard as an array inside of a Gameboard object
-    3. Set up your HTML
-        write a JavaScript function that will render the contents of the gameboard array to the webpage
+function Player(playerNo, name, sign) {
+    playerNo,
+        name,
+        sign,
 
-
-
-
-
-
-*/
-
-const Gameboard = (() => {
-    const gameboard = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
-
-    const render = () => {
-        let consoleGameBoard = '';
-        for (let i = 0; i < 9; i++) {
-            consoleGameBoard += gameboard[i];
-            if (i == 2 || i == 5) {
-                consoleGameBoard += '\n';
-            }
+        getSign: function () {
+            return sign;
+        },
+        getName: function () {
+            return name;
         }
-        console.log(consoleGameBoard);
-    };
-
-    return render;
-})();
-
-Gameboard();
+        getPlayerNo: function () {
+            return playerNo;
+        };
+}
