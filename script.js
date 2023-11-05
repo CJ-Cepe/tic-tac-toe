626bedd627aa1c27c8f53cad912ae33e178ce491
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-06 00:19:26 +0800

    set factory functions and IIEF for the game

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
+//factory function
 function Player(playerNo, name, sign) {
-    playerNo,
-        name,
-        sign,
+    function getPlayerNo() {
+        return playerNo;
+    }
 
-        getSign: function () {
-            return sign;
-        },
-        getName: function () {
-            return name;
-        }
-        getPlayerNo: function () {
-            return playerNo;
-        };
+    function getName() {
+        return name;
+    }
+
+    function getSign() {
+        return sign;
+    }
+
+    return { getPlayerNo, getName, getSign };
 }
+
+//factory function
+function Scoreboard() {
+    let round = 0,
+        p1 = 0,
+        p2 = 0,
+        tie = 0;
+
+    //setters, trying different forms of function
+    const addRound = () => {
+        round++;
+    };
+    const addP1 = () => p1++;
+    const addP2 = function () {
+        p2++;
+    };
+    function addTie() {
+        tie++;
+    }
+
+    //getters
+    const getRound = () => round;
+    const getP1 = () => p1;
+    const getP2 = () => p2;
+    const getTie = () => tie;
+
+    return { addRound, addP1, addP2, addTie, getRound, getP1, getP2, getTie };
+}
+
+function Gameboard() {
+    const board = ['', '', '', '', '', '', '', '', ''];
+
+    const setBoard = () => {};
+    const render = () => {};
+
+    return { render, setBoard };
+}
+
+//IIFE
+const Game_Controller = (() => {})();
