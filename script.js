72139b46a73b6afa8fe8ac8d92452feaa8eb12af
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-11-07 15:30:35 +0800

    decide game winner, update scoreboard, and reset board

--- script.js
+++ script.js
@@ -54,6 +54,19 @@ function Gameboard() {
         board[index] = sign;
         console.log(board);
     };
+
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = '';
+        }
+        let tiles = document.querySelectorAll('.tile');
+        tiles.forEach((tile) => {
+            if (!tile.classList.remove('taken')) {
+                tile.textContent = board[tile.dataset.index];
+            }
+        });
+    };
+
     const render = () => {
         let tiles = document.querySelectorAll('.tile');
         tiles.forEach((tile) => {
@@ -63,7 +76,7 @@ function Gameboard() {
         });
     };
 
-    return { render, setBoard, getBoard };
+    return { render, setBoard, getBoard, resetBoard };
 }
 
 //regular function
@@ -79,13 +92,14 @@ function checkWin(board) {
         [2, 4, 6],
     ];
 
-    winningCombinations.forEach((combination) => {
-        let [a, b, c] = combination;
-
+    for (let i = 0; i < winningCombinations.length; i++) {
+        let [a, b, c] = winningCombinations[i];
+        console.log(board[a], board[b], board[c]);
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            console.log('same');
             return board[a];
         }
-    });
+    }
 
     return null;
 }
@@ -96,23 +110,38 @@ const Game_Controller = (() => {
     //Start-EL
     let p1 = Player('1', 'Sigma', 'X'),
         p2 = Player('2', 'Winston', 'O');
+    let gameBoard = Gameboard();
+    let scoreBoard = Scoreboard();
     var currentPlayer = null;
     //enable board
 
-    if (p1.getSign == 'X') {
-        currentPlayer = p1;
-    } else {
-        currentPlayer = p2;
-    }
+    currentPlayer = p1.getSign() === 'X' ? p1 : p2;
 
-    let gameBoard = Gameboard();
-    console.log(p1.getSign(), p2.getSign());
     let tiles = document.querySelectorAll('.tile');
+
     tiles.forEach((tile) => {
         tile.addEventListener('click', (e) => {
             gameBoard.setBoard(currentPlayer.getSign(), tile.dataset.index);
             gameBoard.render();
             tile.classList.add('taken');
+            let signWinner = checkWin(gameBoard.getBoard());
+            if (signWinner) {
+                console.log(
+                    `${currentPlayer.getName()} P${currentPlayer.getPlayerNo()} Wins!!!`
+                    //restart
+                );
+
+                if (currentPlayer.getPlayerNo() === 1) {
+                    scoreBoard.addP1();
+                } else {
+                    scoreBoard.addP2();
+                }
+
+                gameBoard.resetBoard();
+                console.log('resetBoard');
+                gameBoard.render();
+                console.log('renderboard');
+            }
         });
     });
 })();
